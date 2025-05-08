8e406bb42a3c9398c10dfe876846857f772a7bd3
Luyanda Xhakaza <lloyiswa2003@gmail.com>
2025-05-08 09:26:46 +0200

    created folder for model and added user class (model)

---
+++ spring-boot-user-demo/src/main/java/com/example/model/User.java
@@ -0,0 +1,18 @@
+package com.example.model;
+
+public class User {
+    private long id;
+    private String name;
+    private String surname;
+
+    //constructor
+    public User(long id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
+    //getters and setters
+    
+
+
+}
