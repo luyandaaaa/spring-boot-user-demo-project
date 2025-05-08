406524bf8de9a2272c3490ed642a8edff5034151
Luyanda Xhakaza <lloyiswa2003@gmail.com>
2025-05-08 09:44:58 +0200

    Revert "feat: created fake repo interface with insert user, find user by id and delete user methods"
    This reverts commit 834ac438f91006607cd362b3d7a070d0a41359bd.

--- spring-boot-user-demo/src/main/java/com/example/repo/FakeRepoInterface.java
+++
@@ -1,8 +0,0 @@
-package com.example.repo;
-
-public interface FakeRepoInterface {
-    String insertUser(long id, String name, String surname);
-    String findUserById(long id);
-    String deleteUser(long id);
-
-}
