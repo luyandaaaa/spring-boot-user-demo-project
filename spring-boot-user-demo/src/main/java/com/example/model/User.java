fa12ab49aa6f4c82df9efc694f973c0e10d250e8
Luyanda Xhakaza <lloyiswa2003@gmail.com>
2025-05-08 09:28:17 +0200

    added get and setters methods

--- spring-boot-user-demo/src/main/java/com/example/model/User.java
+++ spring-boot-user-demo/src/main/java/com/example/model/User.java
@@ -12,6 +12,24 @@ public class User {
         this.surname = surname;
     }
     //getters and setters
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }   
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
     
 
 
