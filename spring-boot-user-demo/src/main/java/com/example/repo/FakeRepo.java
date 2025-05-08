ac19e8d8c7df0f0603b0171725efb7fb9365bc5b
Luyanda Xhakaza <lloyiswa2003@gmail.com>
2025-05-08 10:06:08 +0200

    feat: implemented override method of the  insertUser

--- spring-boot-user-demo/src/main/java/com/example/repo/FakeRepo.java
+++ spring-boot-user-demo/src/main/java/com/example/repo/FakeRepo.java
@@ -8,6 +8,17 @@ public class FakeRepo implements FakeRepoInterface {
     private User[] users = new User[10];
     private int userCount = 0;
 
+    @Override
+    public String insertUser(long id, String name, String surname) {
+        if (userCount < users.length) {
+            users[userCount++] = new User(id, name, surname);
+            return name + " added";
+        }
+        return "Database full";
+    }
+
+    
+
 }
 
 
