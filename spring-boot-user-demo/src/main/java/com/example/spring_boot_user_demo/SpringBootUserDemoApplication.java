a01a82114923b99943e058b8842b9632508ab402
Luyanda Xhakaza <lloyiswa2003@gmail.com>
2025-05-08 09:04:05 +0200

    initialize the project with gradle and spring boot depedency

---
+++ spring-boot-user-demo/src/main/java/com/example/spring_boot_user_demo/SpringBootUserDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_user_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUserDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUserDemoApplication.class, args);
+	}
+
+}
